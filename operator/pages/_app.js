6d77df738d81465cdb72292b5067bad77c85425e
Jakub Król <jakub.krol@mc2innovations.com>
2020-11-27 19:02:02 +0100

    unused code removed

--- operator/pages/_app.js
+++ operator/pages/_app.js
@@ -1,7 +1,18 @@
+import React from "react";
+import Head from "next/head";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'leaflet/dist/leaflet.css'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Operator App</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
