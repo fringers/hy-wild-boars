ea9b6b99b977326c081144e5d544961e922f09bd
robi24 <robi240@gmail.com>
2020-11-27 22:33:36 +0000

    feat: add routing transition

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { AnimatedSwitch } from 'react-router-transition';
 import { makeStyles } from '@material-ui/core';
 
 import Intro from './Intro';
@@ -13,6 +14,12 @@ const useStyles = makeStyles(() => ({
     width: '100%',
     margin: 0,
   },
+  switchWrapper: {
+    position: 'relative',
+    '> div': {
+      position: 'absolute',
+    },
+  },
 }));
 
 const App = () => {
@@ -21,11 +28,16 @@ const App = () => {
   return (
     <div className={classes.container}>
       <BrowserRouter>
-        <Switch>
+        <AnimatedSwitch
+          atEnter={{ opacity: 0 }}
+          atLeave={{ opacity: 0 }}
+          atActive={{ opacity: 1 }}
+          className={classes.switchWrapper}
+        >
           <Route exact path="/" component={Intro} />
           <Route exact path="/submit" component={SubmitForm} />
           <Route exact path="/thankyou" component={ThankYou} />
-        </Switch>
+        </AnimatedSwitch>
       </BrowserRouter>
     </div>
   );
