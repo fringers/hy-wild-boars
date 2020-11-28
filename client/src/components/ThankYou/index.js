f612343b4e9bde170cebe6ef28bb7a8e36673dda
robi24 <robi240@gmail.com>
2020-11-28 16:14:51 +0000

    refactor: remove the old components

--- client/src/components/ThankYou/index.js
+++ client/src/components/ThankYou/index.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Confetti from 'react-confetti';
+import { useHistory } from 'react-router-dom';
 import { Button } from '@material-ui/core';
 
-const ThankYou = () => (
-  <div>
-    <Button
-      onClick={() => {
-        console.log('click');
-      }}
-    >
-      THANK YOU PAGE
-    </Button>
-  </div>
-);
+const ThankYou = () => {
+  const history = useHistory();
+
+  return (
+    <div>
+      <Confetti width={window.innerWidth} height={window.innerHeight} />
+      <Button onClick={() => history.push('/home')}>Wróć</Button>
+    </div>
+  );
+};
 
 export default ThankYou;
