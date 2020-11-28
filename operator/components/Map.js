af653797eba2ec11fb37022868235607f6018798
Aleksander Surman <aleksander.surman@mc2innovations.com>
2020-11-28 06:57:44 +0100

    added areas to map

--- operator/components/Map.js
+++ operator/components/Map.js
@@ -1,11 +1,14 @@
-import React, {useRef, useState} from "react";
-import {MapContainer, Marker, TileLayer, LayerGroup, useMapEvents} from "react-leaflet";
+import React, {useRef, useState, useEffect } from "react";
+import {MapContainer, Marker, TileLayer, LayerGroup, Polygon, useMapEvents, LayersControl} from "react-leaflet";
 import "./L.Icon.Pulse.js"
 import "@ansur/leaflet-pulse-icon/dist/L.Icon.Pulse.css"
+import {protectionArea} from "./data/ProtectionArea"
+import {restrictedArea} from "./data/RestrictedArea"
+import {hazardArea} from "./data/HazardArea"
+import {disabledArea} from "./data/DisabledArea"
 
 const defaultCenter = [52.04, 19.67]
 const defaultZoom = 6
-
 
 const newIcon = L.icon.pulse({
     fillColor: 'red',
@@ -55,6 +58,7 @@ const Map = ({requests, center, zoom}) => {
     const mapZoom = zoom || defaultZoom
     const [markersVisible, setMarkersVisible] = useState(true)
 
+
     const statusToIcon = (status) => {
         switch (status) {
           case 'NEW':
@@ -102,6 +106,97 @@ const Map = ({requests, center, zoom}) => {
                         })}
                 </LayerGroup>
                 : ""}
+
+<LayersControl position="topright">
+    <LayersControl.Overlay name="Obszar ochronny (Protection Area)">
+    <LayerGroup>
+
+    {
+        protectionArea?.map( area => {
+            return (
+                <Polygon 
+                positions={area.path.split("|").map(p => p.split(","))} 
+                pathOptions ={{ 
+                    color: area.strokeColor,
+                    weight: area.strokeWeight,
+                    opacity: area.strokeColor,
+                    fillColor: area.fillColor,
+                    fillOpacity: area.fillOpacity,
+                    fillOpacity: area.fillOpacity
+                }} />
+            )
+        }
+        )
+    }
+    </LayerGroup>
+    </LayersControl.Overlay>
+    <LayersControl.Overlay name="Obszar objęty ograniczeniami (Restricted Area)">
+    <LayerGroup>
+
+    {
+        restrictedArea?.map( area => {
+            return (
+                <Polygon 
+                positions={area.path.split("|").map(p => p.split(","))} 
+                pathOptions ={{ 
+                    color: area.strokeColor,
+                    weight: area.strokeWeight,
+                    opacity: area.strokeColor,
+                    fillColor: area.fillColor,
+                    fillOpacity: area.fillOpacity,
+                    fillOpacity: area.fillOpacity
+                }} />
+            )
+        }
+        )
+    }
+    </LayerGroup>
+    </LayersControl.Overlay>
+    <LayersControl.Overlay name="Obszar zagrożenia (Hazard Area)">
+    <LayerGroup>
+
+    {
+        hazardArea?.map( area => {
+            return (
+                <Polygon 
+                positions={area.path.split("|").map(p => p.split(","))} 
+                pathOptions ={{ 
+                    color: area.strokeColor,
+                    weight: area.strokeWeight,
+                    opacity: area.strokeColor,
+                    fillColor: area.fillColor,
+                    fillOpacity: area.fillOpacity,
+                    fillOpacity: area.fillOpacity
+                }} />
+            )
+        }
+        )
+    }
+    </LayerGroup>
+    </LayersControl.Overlay>
+    <LayersControl.Overlay name="Obszar wyłączony">
+    <LayerGroup>
+
+    {
+        disabledArea?.map( area => {
+            return (
+                <Polygon 
+                positions={area.path.split("|").map(p => p.split(","))} 
+                pathOptions ={{ 
+                    color: area.strokeColor,
+                    weight: area.strokeWeight,
+                    opacity: area.strokeColor,
+                    fillColor: area.fillColor,
+                    fillOpacity: area.fillOpacity,
+                    fillOpacity: area.fillOpacity
+                }} />
+            )
+        }
+        )
+    }
+    </LayerGroup>
+    </LayersControl.Overlay>
+</LayersControl>
         </MapContainer>
     )
 }
