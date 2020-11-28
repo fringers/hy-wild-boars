2c4fdd3d52a89ee56cfb49891700bb5bb4dd9dbc
robi24 <robi240@gmail.com>
2020-11-28 20:39:33 +0000

    feat: update sw

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,58 +1,76 @@
-var CACHE_NAME = 'boar-alert-app';
-const RUNTIME = 'runtime';
+importScripts(
+  'https://storage.googleapis.com/workbox-cdn/releases/5.1.4/workbox-sw.js'
+);
 
-var urlsToCache = [
-  '/',
-  '/notifications',
-  '/notifications/*',
-  '/styles/main.css',
-  '/script/main.js'
-];
+const { registerRoute } = workbox.routing;
+const { CacheFirst, StaleWhileRevalidate } = workbox.strategies;
+const { CacheableResponse } = workbox.cacheableResponse;
+const { ExpirationPlugin } = workbox.expiration;
 
-// The install handler takes care of precaching the resources we always need.
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-      .then(self.skipWaiting())
+registerRoute(
+  /\.(?:png|gif|jpg|jpeg|svg)$/,
+  new CacheFirst({
+    cacheName: 'images',
+    plugins: [
+      new ExpirationPlugin({
+        maxEntries: 60, // Only cache 60 requests
+        maxAgeSeconds: 7 * 24 * 60 * 60, // Only cache requests for a week
+      }),
+    ],
+  })
+);
+
+registerRoute(
+  ({ url }) => url.pathname.includes('firestore.googleapis.com'),
+  new StaleWhileRevalidate({
+    cacheName: 'api-cache',
+    plugins: [
+      new CacheableResponse({
+        headers: {
+          'X-Is-Cacheable': 'true',
+        },
+      }),
+    ],
+  })
+);
+
+registerRoute(
+  /\.(?:ttf|woff|otf|svg|eot|woff2)$/,
+  new CacheFirst({
+    cacheName: 'fonts',
+    maxAgeSeconds: 31536000, // 1 year
+  })
+);
+
+self.addEventListener('push', function (event) {
+  console.log('[Service Worker] Push Received.');
+  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+
+  const title = 'Test';
+  const options = {
+    image: 'app_logo.png',
+    badge: 'app_logo.png',
+    icon: 'favicon.ico',
+    body: event.data.text(),
+  };
+
+  const notificationPromise = self.registration.showNotification(
+    title,
+    options
   );
+  event.waitUntil(notificationPromise);
 });
 
-// The activate handler takes care of cleaning up old caches.
-self.addEventListener('activate', event => {
-  const currentCaches = [CACHE_NAME, RUNTIME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
-    }).then(cachesToDelete => {
-      return Promise.all(cachesToDelete.map(cacheToDelete => {
-        return caches.delete(cacheToDelete);
-      }));
-    }).then(() => self.clients.claim())
-  );
+self.addEventListener('notificationclick', function (event) {
+  console.log('[Service Worker] Notification click Received.');
+  event.notification.close();
 });
 
-// The fetch handler serves responses for same-origin resources from a cache.
-// If no response is found, it populates the runtime cache with the response
-// from the network before returning it to the page.
-self.addEventListener('fetch', event => {
-  // Skip cross-origin requests, like those for Google Analytics.
-  if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(
-      caches.match(event.request).then(cachedResponse => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
+self.addEventListener('notificationclose', function (event) {
+  // https://developers.google.com/web/fundamentals/push-notifications/common-notification-patterns
 
-        return caches.open(RUNTIME).then(cache => {
-          return fetch(event.request).then(response => {
-            // Put a copy of the response in the runtime cache.
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
-      })
-    );
-  }
+  // const dismissedNotification = event.notification;
+  // const promiseChain = notificationCloseAnalytics();
+  // event.waitUntil(promiseChain);
+  console.log('[Service Worker] Notification close Received.');
 });
