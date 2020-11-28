72c765bfc7d40b3d8a680a488f6f5e0a62db9f8c
Jakub Król <jakub.krol@mc2innovations.com>
2020-11-28 22:19:58 +0100

    request menu added

---
+++ client/src/components/NotificationDetails/components/RequestMenu.js
@@ -0,0 +1,53 @@
+import React, {useState} from "react";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import {IconButton} from "@material-ui/core";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faEllipsisH} from "@fortawesome/free-solid-svg-icons";
+import {updateRequestStatus} from "../../../firebase/db";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from '@material-ui/core/Menu';
+
+export const RequestMenu = ({request, onCancelled}) => {
+  if (!request || request.status !== 'NEW')
+    return null
+
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleRequestCancel = async () => {
+    setLoading(true);
+    try {
+      await updateRequestStatus(request.id, 'REJECTED');
+      await onCancelled(request.id);
+      setAnchorEl(null);
+    } catch (e) {
+      console.error(e);
+    }
+    setLoading(false);
+  };
+
+  return (
+    <ListItemIcon>
+      <IconButton aria-controls="request-menu" aria-haspopup="true" onClick={handleClick} disabled={loading}>
+        <FontAwesomeIcon icon={faEllipsisH}/>
+      </IconButton>
+      <Menu
+        id="request-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleRequestCancel}>Anuluj</MenuItem>
+      </Menu>
+    </ListItemIcon>
+  )
+}
