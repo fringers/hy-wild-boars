de5316aa1b3485d020e655a96d2b0c92e22fbbd3
Aleksander Surman <aleksander.surman@mc2innovations.com>
2020-11-28 03:25:43 +0100

    added maps icon and animation

--- operator/components/Map.js
+++ operator/components/Map.js
@@ -1,38 +1,106 @@
-import React from "react";
-import {MapContainer, Marker, TileLayer} from "react-leaflet";
+import React, {useRef, useState} from "react";
+import {MapContainer, Marker, TileLayer, LayerGroup, useMapEvents} from "react-leaflet";
+import "./L.Icon.Pulse.js"
+import "@ansur/leaflet-pulse-icon/dist/L.Icon.Pulse.css"
 
-const defaultCenter = [52.04,19.67]
+const defaultCenter = [52.04, 19.67]
 const defaultZoom = 6
 
-const icon = L.icon({
-  iconUrl: '/map-marker.svg',
-  iconSize:     [32, 32],
-  iconAnchor:   [16, 32]
+
+const newIcon = L.icon.pulse({
+    fillColor: 'red',
+    color: 'red',
+    animate: true
 })
 
+const acceptedIcon = L.icon.pulse({
+    fillColor: 'orange',
+    color: 'orange',
+    animate: false
+})
+
+const doneIcon = L.icon.pulse({
+    fillColor: 'green',
+    color: 'green',
+    animate: false
+})
+
+const rejectedIcon = L.icon.pulse({
+    fillColor: 'black',
+    color: 'black',
+    animate: false
+})
+
+
+
+const MyComponent = ({setMarkersVisible}) => {
+    const map = useMapEvents({
+            'zoomend': () => {
+                if (map.getZoom() > 7) {
+                    setMarkersVisible(true)
+                } else {
+                    setMarkersVisible(true)
+                }
+
+            }
+        }
+    )
+
+    return null
+}
+
+
 const Map = ({requests, center, zoom}) => {
-  const mapCenter = center || defaultCenter
-  const mapZoom = zoom || defaultZoom
+    const mapCenter = center || defaultCenter
+    const mapZoom = zoom || defaultZoom
+    const [markersVisible, setMarkersVisible] = useState(true)
 
-  return (
-    <MapContainer center={mapCenter} zoom={mapZoom} className="map">
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-
-      {
-        requests?.map(r => {
-          return (
-            <Marker
-              key={r.id}
-              position={[r.location.latitude, r.location.longitude]}
-              icon={icon}
-            />
-          )
-        })
+    const statusToIcon = (status) => {
+        switch (status) {
+          case 'NEW':
+            return newIcon
+          case 'ACCEPTED':
+            return acceptedIcon
+          case 'REJECTED':
+            return rejectedIcon
+          case 'RESOLVED':
+            return doneIcon
+          default:
+            return doneIcon
+        }
       }
-    </MapContainer>
-  )
+
+    return (
+        <MapContainer center={mapCenter} zoom={mapZoom} className="map">
+            <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <MyComponent setMarkersVisible={setMarkersVisible}/>
+            {markersVisible ?
+                <LayerGroup>
+                    {
+                        requests?.map(r => {
+                            return (
+                                <Marker
+                                    key={r.id}
+                                    position={[r.location.latitude, r.location.longitude]}
+                                    icon={statusToIcon(r.status)}
+                                    eventHandlers={{
+                                        click: (event) => {
+                                            event.target._map.flyTo(event.target._latlng, 15, {
+                                                animate: true,
+                                                duration: 1
+                                            })
+
+                                        },
+                                    }}
+                                />
+                            )
+                        })}
+                </LayerGroup>
+                : ""}
+        </MapContainer>
+    )
 }
 export default Map;
